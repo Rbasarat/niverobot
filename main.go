b0840b0d29a2c21027bd0e0d32d28f914479090f
Rasjaad <rasjaad10@msn.com>
2021-09-06 12:31:00 +0200

    added db connection + migration

--- main.go
+++ main.go
@@ -1,18 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
+	"niverobot/model"
 	"os"
+	"strconv"
 	"strings"
 )
 
+func getEnvString(name string, value string) string {
+	if envValue, ok := os.LookupEnv(name); ok {
+		value = envValue
+	}
+	return value
+}
+func getEnvInt(name string, value int) int {
+	if envValue, ok := os.LookupEnv(name); ok {
+		if intVal, err := strconv.Atoi(envValue); err != nil {
+			value = intVal
+		}
+	}
+	return value
+}
+
+var botToken = getEnvString("BOT_API_TOKEN", "secret")
+
+var dbHost = getEnvString("DB_HOST", "localhost")
+var dbPort = getEnvInt("DB_PORT", 5432)
+var dbUser = getEnvString("DB_USER", "postgres")
+var dbPassword = getEnvString("DB_PASSWORD", "postgres")
+var dbSchema = getEnvString("DB_SCHEMA", "postgres")
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_API_TOKEN"))
+	// TODO: move init db / cleanup
+	log.Print("Connecting to database...")
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+		dbHost,
+		dbUser,
+		dbPassword,
+		dbSchema,
+		dbPort,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Panic(err)
+		log.Panicf("failed to connect to database: %s", err)
 	}
 
+	log.Println("Applying migrations...")
+	err = db.AutoMigrate(&model.User{}, &model.Kudo{})
+	if err != nil {
+		log.Panicf("migration failed: %s", err)
+	}
+
+	log.Println("Connecting to Telegram...")
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Panicf("Failed connecting to Telegram: %s",err)
+	}
+
+	// TODO: check this flag
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
@@ -22,6 +73,7 @@ func main() {
 
 	updates, err := bot.GetUpdatesChan(u)
 
+	// TODO: move this
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
@@ -29,21 +81,24 @@ func main() {
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-		if strings.Contains(strings.ToLower(update.Message.Text), "werkt de bot van siwa"){
+		if update.Message.Chat.ID == 859571171 && update.Message.ReplyToMessage != nil {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "I see your reply")
+			bot.Send(msg)
+		}
+
+		if strings.Contains(strings.ToLower(update.Message.Text), "werkt de bot van siwa") {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Nee, Fix je bot homo!")
 			//msg.ReplyToMessageID = update.Message.MessageID
 
 			bot.Send(msg)
 		}
 
-		if strings.Contains(strings.ToLower(update.Message.Text), "ping"){
+		if strings.Contains(strings.ToLower(update.Message.Text), "ping") {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "pong")
 			//msg.ReplyToMessageID = update.Message.MessageID
 
 			bot.Send(msg)
 		}
-
-
 
 	}
 }
