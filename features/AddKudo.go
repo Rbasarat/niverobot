21fc6986ffa81b243c4aff80b91c7bee220a99f8
Rasjaad <rasjaad10@msn.com>
2021-09-09 21:18:39 +0200

    added voting on kudo rule

--- features/AddKudo.go
+++ features/AddKudo.go
@@ -22,7 +22,6 @@ func NewAddKudo(kudosService model.Kudos, userService model.Users, kudoCountServ
 
 func (k AddKudo) Execute(update tgbotapi.Update, db *gorm.DB, bot *tgbotapi.BotAPI) {
 	var msg tgbotapi.MessageConfig
-
 	receiver, err := k.users.CreateUserIfNotExist(update.Message.ReplyToMessage.From, db)
 	if err != nil && err != gorm.ErrRecordNotFound {
 		msg = tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Kudo error: %s", err))
@@ -31,8 +30,18 @@ func (k AddKudo) Execute(update tgbotapi.Update, db *gorm.DB, bot *tgbotapi.BotA
 	_, err = k.users.CreateUserIfNotExist(update.Message.From, db)
 
 	// You may not vote on your own message.
+	if strings.EqualFold(update.Message.ReplyToMessage.Text, "+") || strings.EqualFold(update.Message.ReplyToMessage.Text, "-") {
+		err = errors.New("voting on a kudo not allowed")
+		msg = tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Kudo error: %s", err))
+		_, err = bot.Send(msg)
+		if err != nil {
+			log.Printf("error sending message %s\n", err)
+		}
+		return
+	}
 	if update.Message.From.ID == update.Message.ReplyToMessage.From.ID {
 		err = errors.New("voting on own message not allowed")
+		msg = tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Kudo error: %s", err))
 		_, err = bot.Send(msg)
 		if err != nil {
 			log.Printf("error sending message %s\n", err)
