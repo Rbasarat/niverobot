56f398d57d06d5e3ded4d3b9c758ae6ea457c7b0
Rasjaad <rasjaad10@msn.com>
2021-10-01 17:48:03 +0200

    Rename action

--- features/AddKudoFromReply.go
+++ features/AddKudoFromReply.go
@@ -53,7 +53,7 @@ func (k AddKudo) Execute(update tgbotapi.Update, db *gorm.DB, bot *tgbotapi.BotA
 		msg = tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Kudo error: %s", err))
 	}
 
-	kudo, err := k.kudos.CreateKudoIfNotExist(update.Message, db)
+	kudo, err := k.kudos.CreateFromReplyKudoIfNotExist(update.Message, db)
 	if err != nil && err != gorm.ErrRecordNotFound {
 		msg = tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Kudo error: %s", err))
 		_, err = bot.Send(msg)
